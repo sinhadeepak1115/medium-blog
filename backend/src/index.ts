79f40f1b8111e4cd95b2190210075ec947b196b7
Deepak Kumar Sinha <94794077+sinhadeepak1115@users.noreply.github.com>
2024-03-05 21:35:38 +0530

    Completed access authenticated routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,15 @@
 import { Hono } from 'hono'
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
-import { sign } from 'hono/jwt'
+import { sign, verify } from 'hono/jwt'
 
 const app = new Hono<{
   Bindings: {
     DATABASE_URL: string,
     JWT_SECRET: string
+  },
+  Variables: {
+    userId: string
   }
 }>();
 
@@ -28,14 +31,53 @@ app.post('/api/v1/user/signup', async (c) => {
     }, c.env.JWT_SECRET);
     return c.text(jwt);
   } catch (e) {
-    console.log(c.env.DATABASE_URL)
     console.log(e);
     return c.status(403);
   }
 })
 
-app.post('/api/v1/user/signin', (c) => {
-  return c.text('signin route')
+app.post('/api/v1/user/signin', async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env?.DATABASE_URL,
+  }).$extends(withAccelerate())
+  const body = await c.req.json();
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        username: body.username
+      }
+    });
+    if (!user) {
+      c.status(403);
+      return c.json({ error: "user not found" })
+    }
+    const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
+    return c.text(jwt);
+  } catch (e) {
+    console.log(e)
+    return c.status(403)
+  }
+})
+
+app.use('/api/v1/blog/*', async (c, next) => {
+  const authHeader = c.req.header("authorization") || "";
+  const header = authHeader.split(" ")[1]
+
+  console.log(header)
+  console.log(authHeader)
+
+  try {
+    const user = await verify(header, c.env?.JWT_SECRET)
+    console.log("next router");
+    if (user) {
+      await next()
+    }
+  } catch (e) {
+    console.log(e)
+    c.status(403)
+    return c.json({ error: "Couldn't move to the next router" })
+
+  }
 })
 
 app.post('/api/v1/blog', (c) => {
